42750f2b6ccb0d7c79787795d290ed7b54fd3483
Maximilien Noal <mnoal@epsi.fr>
2011-10-07 12:48:27 +0200

    tests a part dans un fichier
    Signed-off-by: Maximilien Noal <mnoal@epsi.fr>

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -24,34 +24,4 @@ class FizzBuzzer
 		result = (int%5).zero? ? result + "Buzz" : result
 		return result
 	end
-
-end
-
-class TestMyObject < Test::Unit::TestCase
-
-	def initialize(arg)
-		initialize_fizzbuzzer
-		super
-	end
-
-	def initialize_fizzbuzzer
-		@myfizzbuzobj = FizzBuzzer.new
-	end
-
-	def test_gives_a_hash_of_one_hundred_pairs
-		assert_not_nil @myfizzbuzobj.fizzbuzz
-		assert_equal Hash, @myfizzbuzobj.fizzbuzz.class
-		assert_equal 100, @myfizzbuzobj.fizzbuzz.size
-	end
-
-	def test_is_fizzbuzz
-		hfb = @myfizzbuzobj.fizzbuzz
-		for i in Range.new(1, 100)
-			assert_not_nil hfb[i]
-			assert_equal "Fizz", hfb[i] if (i%3).zero? and !(i%15).zero?
-			assert_equal "Buzz", hfb[i] if (i%5).zero? and !(i%15).zero?
-			assert_equal "FizzBuzz", hfb[i] if (i%15).zero?
-			assert_equal "", hfb[i] if !(i%3).zero? and !(i%5).zero?
-		end
-	end
 end
